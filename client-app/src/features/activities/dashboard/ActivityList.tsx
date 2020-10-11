22a0652ca91e46d4d340b7e4ba889154662dca0a
murphferg <kevinfe@magenic.com>
2020-10-11 15:25:29 -0400

    Debugging mobx

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,22 +1,22 @@
-import React, { SyntheticEvent } from "react";
+import { observer } from "mobx-react-lite";
+import React, { SyntheticEvent, useContext } from "react";
 import { Item, Button, Label, Segment } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
+import ActivityStore from "../../../app/stores/activityStore"
 
 interface IProps {
-  activities: IActivity[];
-  selectActivity: (id: string) => void;
   deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
   submitting: boolean;
   target: string;
 }
 
 export const ActivityList: React.FC<IProps> = ({
-  activities,
-  selectActivity,
   deleteActivity,
   submitting,
   target,
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {activities, selectActivity, selectedActivity} = activityStore;
   return (
     <Segment clearing>
       <Item.Group divided>
@@ -32,7 +32,8 @@ export const ActivityList: React.FC<IProps> = ({
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => selectActivity(activity.id)}
+                  onClick={() => {selectActivity(activity.id);
+                  alert('activity just selected is: ' + selectedActivity)}}
                   floated="right"
                   content="View"
                   color="blue"
@@ -54,3 +55,6 @@ export const ActivityList: React.FC<IProps> = ({
     </Segment>
   );
 };
+
+
+export default observer(ActivityList)

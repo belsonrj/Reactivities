ef4d1121342b4449318f4063fa10ee4665e3d885
murphferg <kevinfe@magenic.com>
2020-10-11 18:44:10 -0400

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,52 +1,41 @@
-import { observer } from "mobx-react-lite";
-import React, { SyntheticEvent, useContext } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
-import ActivityStore from "../../../app/stores/activityStore"
+import React, { useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-  deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
-  submitting: boolean;
-  target: string;
-}
-
-export const ActivityList: React.FC<IProps> = ({
-  deleteActivity,
-  submitting,
-  target,
-}) => {
+const ActivityList: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {activities, selectActivity, selectedActivity} = activityStore;
+  const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
   return (
     <Segment clearing>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as='a'>{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
-                <div>{activity.city}</div>
-                <div>{activity.venue}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => {selectActivity(activity.id);
-                  alert('activity just selected is: ' + selectedActivity)}}
-                  floated="right"
-                  content="View"
-                  color="blue"
+                  onClick={() => selectActivity(activity.id)}
+                  floated='right'
+                  content='View'
+                  color='blue'
                 />
                 <Button
                   name={activity.id}
                   loading={target === activity.id && submitting}
                   onClick={(e) => deleteActivity(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
+                  floated='right'
+                  content='Delete'
+                  color='red'
                 />
-                <Label content={activity.category} />
+                <Label basic content={activity.category} />
               </Item.Extra>
             </Item.Content>
           </Item>
@@ -56,5 +45,4 @@ export const ActivityList: React.FC<IProps> = ({
   );
 };
 
-
-export default observer(ActivityList)
+export default observer(ActivityList);
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,21 +1,11 @@
-import { observer } from "mobx-react-lite";
-import React, { useContext } from "react";
-import { Button, Card, Image } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
-import ActivityStore from "../../../app/stores/activityStore";
+import React, { useContext } from 'react';
+import { Card, Image, Button } from 'semantic-ui-react';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
-interface IProps {
-  setEditMode: (editMode: boolean) => void;
-  setSelectedActivity: (activity: IActivity | null) => void;
-}
-
-export const ActivityDetails: React.FC<IProps> = ({
-  setEditMode,
-  setSelectedActivity,
-}) => {
+const ActivityDetails: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const {selectedActivity: activity} = activityStore;
-  alert('Hello from ActivityDetails')
+  const { selectedActivity: activity, openEditForm, cancelSelectedActivity } = activityStore;
   return (
     <Card fluid>
       <Image
@@ -23,7 +13,6 @@ export const ActivityDetails: React.FC<IProps> = ({
         wrapped
         ui={false}
       />
-
       <Card.Content>
         <Card.Header>{activity!.title}</Card.Header>
         <Card.Meta>
@@ -34,16 +23,16 @@ export const ActivityDetails: React.FC<IProps> = ({
       <Card.Content extra>
         <Button.Group widths={2}>
           <Button
-            onClick={() => setEditMode(true)}
+            onClick={() => openEditForm(activity!.id)}
             basic
-            color="blue"
-            content="Edit"
+            color='blue'
+            content='Edit'
           />
           <Button
-            onClick={() => setSelectedActivity(null)}
+            onClick={cancelSelectedActivity}
             basic
-            color="grey"
-            content="Cancel"
+            color='grey'
+            content='Cancel'
           />
         </Button.Group>
       </Card.Content>
